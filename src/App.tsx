effe2eef9428c0b3bdd5a810757bc38050ebacc4
BogdanFV <fbogdan13@gmail.com>
2023-10-20 21:29:30 +0300

    video implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useRef, useState } from 'react';
 import './App.css';
 
 function App() {
+  const videoRef = useRef<HTMLVideoElement | null>(null);
+  const [showBanner, setShowBanner] = useState(false);
+
+  useEffect(() => {
+    if (videoRef.current) {
+      videoRef.current.addEventListener('play', () => {
+        setTimeout(() => setShowBanner(true), 5000);
+      });
+    }
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <video
+        ref={videoRef}
+        src="./assets/vandam.mp4"
+        autoPlay
+        loop
+        muted
+        width="1280"
+        height="720"
+      />
+      {showBanner && (
+        <div className="banner">
+          баннер
+        </div>
+      )}
     </div>
   );
 }
